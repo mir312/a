8450674f67d3169c75ad795d925021520a9f4ede
mir312 <dakievameerim0@gmail.com>
2024-05-14 00:26:22 +0600

    created footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,9 +2,14 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <>
-      Footer
-    </>
+   <section className='footer'>
+    <div className="container">
+      <div className="footer_block flex">
+  <h2 className="sec_tit">JOIN OUR ADICLUB & GET 15% OFF</h2>
+  <a href="" className="btn_buy">Sing up for free</a>
+      </div>
+    </div>
+   </section>
   );
 }
 
