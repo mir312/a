b95f9a090a8ee1ad90def025daf36fc090f7197c
mir312 <dakievameerim0@gmail.com>
2024-05-16 11:36:27 +0600

    add for footer new styles

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,14 +1,172 @@
 import './Footer.css';
+// import '../../img/icons/ic_face.svg'
 
 function Footer() {
   return (
    <section className='footer'>
     <div className="container">
       <div className="footer_block flex">
-  <h2 className="sec_foot">JOIN OUR ADICLUB & GET 15% OFF</h2>
+  <h2 className="sec_footer">JOIN OUR ADICLUB & GET 15% OFF</h2>
   <a href="" className="btn_buy">Sing up for free</a>
       </div>
+
+      <div className="footer_foot flex">
+
+   <div className="foot">
+   <h3 className="sec_foot">
+   PRODUCTS
+   </h3>
+   <p className="sec_desk">
+Shoes
+   </p>
+   <p className="sec_desk">
+Clothing
+   </p>
+   <p className="sec_desk">
+
+   accessories
+   </p>
+   <p className="sec_desk">
+Shoes
+   </p>
+   <p className="sec_desk">
+Gift cards
+   </p>
+
+   <p className="sec_desk">
+   New Arrivals
+   </p>
+   <p className="sec_desk">
+  Best Sellers
+   </p>
+   <p className="sec_desk">
+Sale
+   </p>
+   </div>
+
+   <div className="foot">
+   <h3 className="sec_foot">
+   COLLECTIONS
+   </h3>
+   <p className="sec_desk">
+Shoes
+   </p>
+   <p className="sec_desk">
+Clothing
+   </p>
+   <p className="sec_desk">
+
+   accessories
+   </p>
+   <p className="sec_desk">
+Shoes
+   </p>
+   <p className="sec_desk">
+Gift cards
+   </p>
+
+   <p className="sec_desk">
+   New Arrivals
+   </p>
+   <p className="sec_desk">
+  Best Sellers
+   </p>
+   <p className="sec_desk">
+Sale
+   </p>
+   </div>
+
+<div className="foot">
+   <h3 className="sec_foot">
+   COMPANY INFO
+   </h3>
+   <p className="sec_desk">
+Shoes
+   </p>
+   <p className="sec_desk">
+Clothing
+   </p>
+   <p className="sec_desk">
+
+   accessories
+   </p>
+   <p className="sec_desk">
+Shoes
+   </p>
+   <p className="sec_desk">
+Gift cards
+   </p>
+
+   <p className="sec_desk">
+   New Arrivals
+   </p>
+   <p className="sec_desk">
+  Best Sellers
+   </p>
+   <p className="sec_desk">
+Sale
+   </p>
+   </div>
+
+   <div className="foot">
+   <h3 className="sec_foot">
+SPORTS
+   </h3>
+   <p className="sec_desk">
+Shoes
+   </p>
+   <p className="sec_desk">
+Clothing
+   </p>
+   <p className="sec_desk">
+
+   accessories
+   </p>
+   <p className="sec_desk">
+Shoes
+   </p>
+   <p className="sec_desk">
+Gift cards
+   </p>
+
+   <p className="sec_desk">
+   New Arrivals
+   </p>
+   <p className="sec_desk">
+  Best Sellers
+   </p>
+   <p className="sec_desk">
+Sale
+   </p>
+   </div>
+
+   <h3 className="sec_foot">FOLLOW US</h3>
+<div className="icons_foot">
+ 
+  <i className="icon_foot face"></i>
+  <i className="icon_foot"></i>
+  <i className="icon_foot"></i>
+  <i className="icon_foot"></i>
+  <i className="icon_foot"></i>
+  <i className="icon_foot"></i>
+</div>
+
+      </div>
+
+     
     </div>
+    <div className="foot_grey">
+    <div className="foot_under flex">
+      <p className="sec_desk">Your Privacy Choices
+</p>
+      <p className="sec_desk">Your Privacy Choices
+</p>
+      <p className="sec_desk">
+Terms and Conditions</p>
+    </div>
+
+    <p className="foot_txt">© 2024 adidas America, Inc.</p>
+      </div>
    </section>
   );
 }
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -30,6 +30,8 @@ function About() {
         </div>
       </div>
     </div>
+
+    
   );
 
 }
