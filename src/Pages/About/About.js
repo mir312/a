3869745cb06a960d07fb43ba12b18de5f943fdb1
mir312 <dakievameerim0@gmail.com>
2024-05-16 12:08:58 +0600

    for about add to cards

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -4,7 +4,7 @@ import './About.css';
 
 function About() {
   return (
-    <div className="cont">
+    <div className="cont flex">
       <div className="card">
         <div className="imgBx">
           <img className='shoes' src="https://assets.codepen.io/4164355/shoes.png" />
@@ -29,11 +29,80 @@ function About() {
           <a href="#">Buy Now</a>
         </div>
       </div>
+
+
+      <div className="card">
+        <div className="imgBx">
+          <img className='shoes' src="https://assets.codepen.io/4164355/shoes.png" />
+
+          {/* <img src="https://kinash.ru/pictures/product/middle/248979_middle.jpg"/> */}
+        </div>
+        <div className="contentBx">
+          <h2 >Adidas Shoes</h2>
+          <div className="size">
+            <h3>Size :</h3>
+            <span>7</span>
+            <span>8</span>
+            <span>9</span>
+            <span>10</span>
+          </div>
+          <div className="color">
+            <h3>Color :</h3>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <a href="#">Buy Now</a>
+        </div>
+      </div>
+
+      <div className="card">
+        <div className="imgBx">
+          <img className='shoes' src="https://assets.codepen.io/4164355/shoes.png" />
+
+          {/* <img src="https://kinash.ru/pictures/product/middle/248979_middle.jpg"/> */}
+        </div>
+        <div className="contentBx">
+          <h2 >Adidas Shoes</h2>
+          <div className="size">
+            <h3>Size :</h3>
+            <span>7</span>
+            <span>8</span>
+            <span>9</span>
+            <span>10</span>
+          </div>
+          <div className="color">
+            <h3>Color :</h3>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <a href="#">Buy Now</a>
+        </div>
+      </div>
+
     </div>
 
-    
+
+
   );
 
 }
+
+  {/* <div className="about ">
+    
+    <h2 className="sec_tit">Still Interested?</h2>   
+
+   <div className="cards flex">
+    
+    <div className="card"><img src="img/adi-1.png" alt="" /></div>
+   <a href=""><h3 className="sec_about">SUPERSTAR</h3></a>
+<p>Style that steals the now </p>
+<a href="" className="shop">SHOP NOW</a>
+   </div>
+
+    </div> */}
+
+
 
 export default About;
