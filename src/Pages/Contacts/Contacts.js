292296d03cc5a93ee1b960050db07811bff2053a
mir312 <dakievameerim0@gmail.com>
2024-05-21 01:08:05 +0600

    created section for page contacts

--- src/Pages/Contacts/Contacts.js
+++ src/Pages/Contacts/Contacts.js
@@ -2,43 +2,50 @@ import './Contacts.css';
 
 function Contacts() {
   return (
-<section class="contacts">
-		<div class="container">
-			<h2 class="page_tit">Book A Table</h2>
-			<p class="page_desc">We consider all the drivers of change gives you the components you need to change to create a truly happens.</p>
-		<form action="" class="form form_book">
-         <div class="form_in flex">
-			<div class="form_input">
-				<span class="form_tit">Date</span>
-				<input type="text" class="input input_txt" placeholder="04/01/2021"/>
+<section className="contacts">
+		<div className="container">
+		
+		<form action="" className="form form_book flex">
+         <div className="form_in flex">
+			<div className="form_input">
+				<span className="form_tit">Date</span>
+				<input type="text" className="input input_txt" placeholder="04/01/2021"/>
 			</div>
-			<div class="form_input">
-				<span class="form_tit">Time</span>
-				<input type="text" class="input input_txt" placeholder="06:30 PM"/>
+			<div className="form_input">
+				<span className="form_tit">Time</span>
+				<input type="text" className="input input_txt" placeholder="06:30 PM"/>
 			</div>
-			<div class="form_input">
-				<span class="form_tit">Name</span>
-				<input type="text" class="input input_txt" placeholder="Enter your name"/>
+			<div className="form_input">
+				<span className="form_tit">Name</span>
+				<input type="text" className="input input_txt" placeholder="Enter your name"/>
 			</div>
-			<div class="form_input">
-				<span class="form_tit">Phone</span>
-				<input type="text" class="input input_txt" placeholder="x-xxx-xxx-xxxx"/>
+			<div className="form_input">
+				<span className="form_tit">Phone</span>
+				<input type="text" className="input input_txt" placeholder="x-xxx-xxx-xxxx"/>
 			</div>
-			<div class="form_input form_input_big">
-				<span class="form_tit">Total Person</span>
-				<input type="text" class="input input_txt" placeholder="1 Person"/>
+			<div className="form_input form_input_big">
+				<span className="form_tit">Total Person</span>
+				<input type="text" className="input input_txt" placeholder="1 Person"/>
 			</div>
 			
 
-			<div class="form_btn">
-				<button class="btn btn_red" type="submit">Book A Table</button>
+			<div className="form_btn">
+				<button className="btn btn_red" type="submit">Enter</button>
 			</div>
 		 </div>
 
+     <div className="form_contacts flex">
+			
+		 </div>
+
+
 		</form>
 		</div>
-       <div class="map">
-		<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ad5a7c4015459fbbd3ccfbc4ce02fea4007a57b71f906ad5e6ea04b38caa25dff&amp;source=constructor" width="100%" height="100%" frameborder="0"></iframe>
+       <div className="map">
+		{/* <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ad5a7c4015459fbbd3ccfbc4ce02fea4007a57b71f906ad5e6ea04b38caa25dff&amp;source=constructor" width="100%" height="100%" frameborder="0"></iframe> */}
+  
+    <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A028dcce4280782e701ccf88250efd937fb3b131caccc0fcaf9a5094a129540b5&amp;source=constructor" width="100%" height="100%" frameborder="0"></iframe>
+  
 
 	   </div>
 	</section>
