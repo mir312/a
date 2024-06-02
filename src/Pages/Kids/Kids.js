b8c6f0bac998f3b48a1cc84e80478ac72b7b6a3b
mir312 <dakievameerim0@gmail.com>
2024-06-02 16:30:42 +0600

    Add text for video

--- src/Pages/Kids/Kids.js
+++ src/Pages/Kids/Kids.js
@@ -8,7 +8,13 @@ function  Kids() {
  <video className='kids_video' controls autoPlay muted>
    <source src={video} type="kids/mp4"></source>
                   </video>
- 
+                  <h2 className="kids_title"> ADIDAS|DISNEY:THE LION KING</h2>
+ <p className="kids_desk">For every brave adventure</p>
+
+ <button className="btn_kids">SHOP NOW</button>
+ <div className="container">
+
+ </div>
  </section>
   );
 }
